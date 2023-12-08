d1445393a744e14b6a97f2bb3b9e674affa069f2
Mhd Basel Bashir <u13472@isoint.net>
2023-12-08 12:17:24 +0300

    adding charts

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -30,10 +30,11 @@ class AdminController extends Controller
         $users = $this->userService->users();
         $orders = $this->orderService->orders();
         $researches = $this->researchService->researches();
+
         return view('admin.pages.dashboard', ['title' => 'لوحة التحكم'], [
             'users' => $users,
             'orders' => $orders,
-            'researches' => $researches
+            'researches' => $researches,
         ]);
     }
 
@@ -58,4 +59,41 @@ class AdminController extends Controller
     {
         return redirect()->route('dashboard');
     }
+
+    // public function charts()
+    // {
+    //     $services = Service::all();
+    //     $subCategories = SubCategory::all();
+    //     $categories = Category::all();
+    //     $clients = Client::all();
+    //     $maintenances = MaintenanceTechnician::all();
+    //     $maintenancesJoinRequests = MaintenanceTechnician::where('is_verified', 0)->get();
+
+    //     $orders = Order::all();
+    //     $finished = Order::where('status', OrderStatus::finished)->get();
+    //     $processing = Order::where('status', OrderStatus::processing)->get();
+    //     $canceled = Order::where('status', OrderStatus::canceled)->get();
+    //     $other = Order::whereIn('status', [OrderStatus::newOrder, OrderStatus::pendingClientApprove, OrderStatus::pendingClientApproveFinish, OrderStatus::pendingMaintenanceConfirm])->get();
+
+    //     $totalPrice = 0.0;
+    //     foreach ($orders as $order) {
+    //         foreach ($order->orderServices as $orderService) {
+    //             $totalPrice += $orderService->service->price * $orderService->quantity;
+    //         }
+    //     }
+
+    //     return view('dashboard', [
+    //         'services' => $services,
+    //         'subCategories' => $subCategories,
+    //         'categories' => $categories,
+    //         'clients' => $clients,
+    //         'maintenances' => $maintenances,
+    //         'maintenancesJoinRequests' => $maintenancesJoinRequests,
+    //         'finished' => $finished,
+    //         'processing' => $processing,
+    //         'canceled' => $canceled,
+    //         'other' => $other,
+    //         'totalPrice' => $totalPrice,
+    //     ]);
+    // }
 }
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -157,7 +157,7 @@
             </div>
         </div> --}}
 
-        <div class="container mt-4 row">
+        <div class="container row m-auto">
             <div class="col-xl-3 col-lg-6 col-md-6 mb-4">
                 <a href="/admin-panel-management/orders">
                     <div class="card bg-secondary">
@@ -226,12 +226,7 @@
         <div class="container mt-4 row">
             <div id="piechart" style="width: 690px; height: 400px;" class="text-right"></div>
             {{-- <div id="columnchart" style="width: 100px; height: 100px;" class="text-left"></div> --}}
-            <div id="chart_div" style="width: 690px; height: 400px;" class="text-right"></div>
-        </div>
-
-        <div class="container mt-4 row">
-            {{-- <div id="chart_div" style="width: 500px; height: 400px;" class="text-right"></div> --}}
-            {{-- <div id="columnchart" style="width: 100px; height: 100px;" class="text-left"></div> --}}
+            {{-- <div id="chart_div" style="width: 690px; height: 400px;" class="text-right"></div> --}}
         </div>
 
     </div>
@@ -257,8 +252,7 @@
         {{-- @vite(['resources/assets/js/widgets/modules-widgets.js']) --}}
 
 
-
-        {{-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
             google.charts.load('current', {
                 'packages': ['corechart']
@@ -271,9 +265,9 @@
                     ['Type', 'Count', {
                         role: 'link'
                     }],
-                    ['تصنيفات رئيسية', <?php echo count($categories); ?>, '/categories'],
-                    ['تصنيفات فرعية', <?php echo count($subCategories); ?>, '/sub-categories'],
-                    ['خدمات', <?php echo count($services); ?>, '/services'],
+                    ['الطلبات', <?php echo count($orders); ?>, '/admin-panel-management/orders'],
+                    ['الطلبات', <?php echo count($orders); ?>, '/admin-panel-management/orders'],
+                    ['الطلبات', <?php echo count($orders); ?>, '/admin-panel-management/orders'],
                 ]);
 
                 var options = {
@@ -296,98 +290,6 @@
             }
         </script>
 
-        <script type="text/javascript">
-            google.charts.load("current", {
-                packages: ['corechart']
-            });
-            google.charts.setOnLoadCallback(drawChart);
-
-            function drawChart() {
-                var data = google.visualization.arrayToDataTable([
-                    ['User Type', 'Count', {
-                        role: 'style',
-                    }, {
-                        role: 'link'
-                    }],
-                    ['Clients', <?php echo count($clients); ?>, '#b87333', '/clients'],
-                    ['Maintenances', <?php echo count($maintenances); ?>, 'silver', '/maintenance-technicians'],
-                ]);
-
-                var view = new google.visualization.DataView(data);
-                view.setColumns([0, 1,
-                    {
-                        calc: "stringify",
-                        sourceColumn: 1,
-                        type: "string",
-                        role: "annotation"
-                    },
-                    2
-                ]);
-
-                var options = {
-                    title: "Users",
-                    width: 600,
-                    height: 400,
-                    bar: {
-                        groupWidth: "95%"
-                    },
-                    legend: {
-                        position: "none"
-                    },
-                };
-                var chart = new google.visualization.ColumnChart(document.getElementById("columnchart"));
-
-                google.visualization.events.addListener(chart, 'select', function() {
-                    var selection = chart.getSelection();
-                    if (selection.length > 0) {
-                        var row = selection[0].row;
-                        var url = data.getValue(row, 3);
-                        window.location.href = url;
-                    }
-                });
-
-                chart.draw(view, options);
-            }
-        </script>
-
-        <script>
-            google.charts.load('current', {
-                'packages': ['corechart']
-            });
-            google.charts.setOnLoadCallback(drawChart);
-
-            function drawChart() {
-
-                var data = new google.visualization.DataTable();
-                data.addColumn('string', 'Pizza');
-                data.addColumn('number', 'Populartiy');
-                data.addColumn(['string', 'Link']);
-                data.addRows([
-                    ['منتهي', <?php echo count($finished); ?>, '/orders'],
-                    ['قيد التحضير', <?php echo count($processing); ?>, '/orders'],
-                    ['ملغي', <?php echo count($canceled); ?>, '/orders'],
-                    ['غير ذلك', <?php echo count($other); ?>, '/orders']
-                ]);
-
-                var options = {
-                    title: 'الطلبات',
-                    sliceVisibilityThreshold: .2,
-                    colors: ['green', 'orange', 'red', 'lightgray']
-                };
-
-                var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-
-                google.visualization.events.addListener(chart, 'select', function() {
-                    var selectedItem = chart.getSelection()[0];
-                    if (selectedItem) {
-                        var link = data.getValue(selectedItem.row, 2);
-                        window.open(link);
-                    }
-                });
-
-                chart.draw(data, options);
-            }
-        </script> --}}
 
 
     </x-slot>
