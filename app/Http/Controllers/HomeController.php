1b78cceb687202a34a26bfe0ce1e02f7903100ee
Mhd Basel Bashir <u13472@isoint.net>
2023-12-12 00:06:05 +0300

    fix HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,45 +38,6 @@ class HomeController extends Controller
         Artisan::call('config:cache');
         Artisan::call('cache:clear');
         Artisan::call('view:clear');
-        // Artisan::call('config:cache && cache:clear && view:clear');
         return 'cleared';
     }
-
-    // public function charts()
-    // {
-    //     $services = Service::all();
-    //     $subCategories = SubCategory::all();
-    //     $categories = Category::all();
-    //     $clients = Client::all();
-    //     $maintenances = MaintenanceTechnician::all();
-    //     $maintenancesJoinRequests = MaintenanceTechnician::where('is_verified', 0)->get();
-
-    //     $orders = Order::all();
-    //     $finished = Order::where('status', OrderStatus::finished)->get();
-    //     $processing = Order::where('status', OrderStatus::processing)->get();
-    //     $canceled = Order::where('status', OrderStatus::canceled)->get();
-    //     $other = Order::whereIn('status', [OrderStatus::newOrder, OrderStatus::pendingClientApprove, OrderStatus::pendingClientApproveFinish, OrderStatus::pendingMaintenanceConfirm])->get();
-
-    //     $totalPrice = 0.0;
-    //     foreach ($orders as $order) {
-    //         foreach ($order->orderServices as $orderService) {
-    //             $totalPrice += $orderService->service->price * $orderService->quantity;
-    //         }
-    //     }
-
-    //     return view('dashboard', [
-    //         'services' => $services,
-    //         'subCategories' => $subCategories,
-    //         'categories' => $categories,
-    //         'clients' => $clients,
-    //         'maintenances' => $maintenances,
-    //         'maintenancesJoinRequests' => $maintenancesJoinRequests,
-    //         'finished' => $finished,
-    //         'processing' => $processing,
-    //         'canceled' => $canceled,
-    //         'other' => $other,
-    //         'totalPrice' => $totalPrice,
-    //     ]);
-    // }
-
 }
