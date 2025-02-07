0d07c84cbe35f88138015bc3b9d7fbc980ddb71d
Basel Bashir <eng.baselbashir@gmail.com>
2025-02-07 23:13:37 +0300

    Fix /checkSceneStatus API

--- app/Http/Controllers/API/SceneController.php
+++ app/Http/Controllers/API/SceneController.php
@@ -4,15 +4,26 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Scene;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SceneController extends Controller
 {
+    /**
+     * @throws Exception
+     */
     public function checkSceneStatus($sceneUnityId): JsonResponse
     {
-        $scene = Scene::where('scene_unity_id', $sceneUnityId)->first();
-        $response = ['supported' => $scene->supported = !0, 'learned' => $scene->learned = !0];
-        return response()->json($response);
+        try {
+            $scene = Scene::where('scene_unity_id', $sceneUnityId)->first();
+            if ($scene == null) {
+                throw new Exception("Scene $sceneUnityId not found");
+            }
+            $response = ['supported' => $scene->supported = !0, 'learned' => $scene->learned = !0];
+            return response()->json($response);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
     }
 }
